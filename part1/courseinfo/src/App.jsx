581630c2b67ba99d6247714d537543793b692c1f
jrmpo <jpoley96@gmail.com>
2025-08-20 12:12:11 -0700

    submission directory for part1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = (props) => {
+ return(
+  <h1>{props.course}</h1>
+ )
+}
+
+const Part = (props) => {
+  console.log(props.section.part)
+  console.log(props.section.exercises)
+  return (
+    <>
+    <p>{props.section.part} {props.section.exercises}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part section={props.parts[0]} />
+      <Part section={props.parts[1]} />
+      <Part section={props.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises: {props.exercises.reduce((accumulator, currentValue) => accumulator + currentValue, 0)} </p>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14 
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={
+        [
+        {part: part1, exercises: exercises1}, 
+        {part: part2, exercises: exercises2}, 
+        {part: part3, exercises: exercises3},
+        ]
+        } />
+      <Total exercises = {[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+export default App
